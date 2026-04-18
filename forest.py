20b22b07eab2d3b2ebde0cfeca80a30effcadf0e
begeritos <72303665+3DMARTIAN@users.noreply.github.com>
2026-04-18 10:59:57 +0700

    Create forest.py

---
+++ forest.py
@@ -0,0 +1,7 @@
+def describe_forest():
+    trees = ["Oak", "Pine", "Birch", "Maple"]
+    for tree in trees:
+        print(f"The forest has many {tree} trees.")
+
+if __name__ == "__main__":
+    describe_forest()
